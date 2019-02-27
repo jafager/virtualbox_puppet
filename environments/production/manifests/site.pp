1133ba551c52e87327346f109ed2f495922b3f2c
Jason A. Fager <jafager@jafager.com>
2019-02-26 19:03:36 -0500

    Added virtualbox_administrators to all nodes

---
+++ environments/production/manifests/site.pp
@@ -0,0 +1,9 @@
+node default
+{
+    class { 'virtualbox_administrators': }
+}
+
+node genesis.jafager.vbox
+{
+    class { 'virtualbox_administrators': }
+}
---
+++ environments/production/modules/virtualbox_administrators/manifests/init.pp
@@ -0,0 +1,67 @@
+class virtualbox_administrators
+{
+
+    group { 'admins':
+        ensure => present,
+        gid => 2100,
+    }
+
+    group { 'jafager':
+        ensure => present,
+        gid => 1100,
+    }
+
+    user { 'jafager':
+        ensure => present,
+        comment => 'Jason A. Fager',
+        uid => 1100,
+        gid => jafager,
+        groups => admins,
+        home => '/home/jafager',
+        managehome => false,
+        password => '*',
+        purge_ssh_keys => true,
+        shell => '/bin/bash',
+        require => [
+            Group['jafager'],
+            Group['admins'],
+        ],
+    }
+
+    file { '/home/jafager':
+        ensure => directory,
+        owner => jafager,
+        group => jafager,
+        mode => '0700',
+        require => [
+            User['jafager'],
+            Group['jafager'],
+        ],
+    }
+
+    file { '/home/jafager/.ssh':
+        ensure => directory,
+        owner => jafager,
+        group => jafager,
+        mode => '0700',
+        require => File['/home/jafager'],
+    }
+
+    ssh_authorized_key { 'jafager@PRISM':
+        ensure => present,
+        user => 'jafager',
+        type => 'ssh-rsa',
+        key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDBWghKioWJSTqcCfNonD8S146NK9f8cprpWGYphxr/08Uv6u/Qlei80OQsVJKy0KKowcI482/0HkHmjbcboEZM3VZvKDlpv3lt3I13rsa0o8LrltqURSoemZzSLXTw04jwpbf3KEkcoAJhczrbUwMCKlsC1PsPXJ1PjeA9cSFLcT2zJjY+Wdy4wxkyN4K1fyXtkasAEbHHrZ3u/qTZRY/ig8/oI+il9sXOZVxbieCLzJL/8tKEgaTtI9XZA7lHY+DLD1tta9+GigvCPRSYdobLnDUi2ub4eMZIuPoLg4qBA2PqazlFStZ52pHOv106V4x7mD0+fmE5ummGORw4VzE/',
+        require => File['/home/jafager/.ssh'],
+    }
+
+    file { '/home/jafager/.hushlogin':
+        ensure => present,
+        owner => jafager,
+        group => jafager,
+        mode => '0600',
+        content => '',
+        require => File['/home/jafager'],
+    }
+
+}
